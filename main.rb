bc327a93ea1557e9612d351ca55a1a8855d04963
Ella Ye <yeella905@gmail.com>
2025-05-10 22:24:47 -0400

    new: added tester codes for finding id

--- main.rb
+++ main.rb
@@ -6,10 +6,16 @@ require './filters'
 
 ## Your test code can go here
 
-pp @candidates
+#pp @candidates
 
-puts "Is candidate 5 experienced?"
-puts experienced?(find(5))  # Should print true
+#puts "Is candidate 5 experienced?"
+#puts experienced?(find(5))  # Should print true
 
-puts "Is candidate 7 experienced?"
-puts experienced?(find(7))  # Should print false
+#puts "Is candidate 7 experienced?"
+#puts experienced?(find(7))  # Should print false
+
+candidate = find(5)
+pp candidate  # Should print the candidate with id 5
+
+not_found = find(100)
+pp not_found  # Should print nil
